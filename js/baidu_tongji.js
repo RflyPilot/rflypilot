09e97696fa7d332c6def335247feb892090d6810
nash.zhao <umview@163.com>
2025-01-15 11:15:47 +0800

    Deployed 552fa18 with MkDocs version: 1.5.3

---
+++ js/baidu_tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?05d00e296ca475d13d1d31c31c93da4e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
